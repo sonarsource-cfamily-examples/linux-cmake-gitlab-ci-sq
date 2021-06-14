5ef291df609cd9900f1aad0fbffda80719853011
Arseniy Zaostrovnykh <arseniy.zaostrovnykh@sonarsource.com>
2021-06-14 17:38:44 +0200

    Add code and boilerplate files

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  int num = argc - 1;
+
+  if (num == 0) {
+    cout << "No arguments provided\n";
+  } else if (num == 0) { // intentional mistake
+    cout << "1 argument provided\n";
+  } else if (num == 2) {
+    cout << "2 arguments provided\n";
+  } else {
+    cout << num << " arguments provided\n";
+  }
+  if (argv != 0) {
+    cout << "argv not null\n";; // intentional extra-semicolon
+  }
+  if (argv == nullptr) {
+    return *argv; // intentional nullptr dereference
+  }
+
+  return 0;
+}
+
